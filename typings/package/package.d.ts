9d6ac9dadfa1cf1b2aee893a944e2edb44ed2b4a
Daranix <daranixlion@gmail.com>
2020-05-24 18:03:35 +0200

    Update definition of package namespace with new method: Package.RequirePackge

--- typings/package/package.d.ts
+++ typings/package/package.d.ts
@@ -5,5 +5,6 @@ declare namespace Package {
     function GetName(): string;
     function Log(message: string): void;
     function Require(luaFile: string): void;
+    function RequirePackage(packageName: string): void;
     function on(eventType: PackageEventType, action: () => void): void;
 }
