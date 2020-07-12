db1e446a14a72db8f2a74e6be5f421e6c793b271
Daranix <daranixlion@gmail.com>
2020-07-12 22:08:53 +0200

    0.31.0 -  Render UpdateItemText and UpdateItemPosition.

--- typings/render/render.d.ts
+++ typings/render/render.d.ts
@@ -8,11 +8,11 @@
 
 /** @noSelf */
 declare namespace Render {
-    function AddLine(id: number, startPosition: Vector2D, endPosition: Vector2D, thickness: number, color: Color): void;
-    function AddBox(id: number, startPosition: Vector2D, endPosition: Vector2D, thickness: number, color: Color): void;
-    function AddPolygon(id: number, startPosition: Vector2D, endPosition: Vector2D, thickness: number, color: Color): void;
+    function AddLine(groupId: number, startPosition: Vector2D, endPosition: Vector2D, thickness: number, color: Color): number;
+    function AddBox(groupId: number, startPosition: Vector2D, endPosition: Vector2D, thickness: number, color: Color): number;
+    function AddPolygon(groupId: number, startPosition: Vector2D, endPosition: Vector2D, thickness: number, color: Color): number;
     function AddText(
-        id: number,
+        groupId: number,
         text: string,
         position: Vector2D,
         fontType: FontType,
@@ -26,12 +26,16 @@ declare namespace Render {
         shadowColor: Color,
         enableOutline: boolean,
         outLineColor: Color
-    ): void;
-    function AddTile(id: number, position: Vector2D, size: Vector2D, color: Color): void;
+    ): number;
+    function AddTile(groupId: number, position: Vector2D, size: Vector2D, color: Color): number;
     function StrLen(text: string, fontType: FontType, fontSize: number): Vector2D;
     function GetViewportSize(): Vector2D;
+    function UpdateItemText(groupId: number, itemId: number, newText: string): void;
+    function UpdateItemPosition(groupId: number, itemId: number, newPosition: Vector2D): void;
 
     function Project(location3d: Vector): Vector2D;
     function ClearItems(id: number): void;
+
+    function on(eventType: RenderEventType.ViewportResized, action: (newSize: Vector2D) => void): void;
 
 }
